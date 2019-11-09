abb93401a9f11d67d287aab82e1c7b60aae73e4d
jhinz <jhinz402@hotmail.com>
2019-11-09 10:25:16 -0500

    Ado Data Sample Initial Commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlConnection connection =
+                new SqlConnection(@"Data Source=CSHARPHUMBER\HUMBERBRIDGING;Initial Catalog=JOB_PORTAL_DB;Integrated Security=True;");
+
+            using (connection)
+            {
+                connection.Open();
+
+                string query = "select * from Applicant_Educations";
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = query;
+
+                ApplicantEducation[] applicantEducations =
+                    new ApplicantEducation[1000];
+                int index = 0;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    ApplicantEducation edu = new ApplicantEducation();
+                    edu.Id = (Guid)reader["Id"];
+                    edu.Applicant = (Guid)reader["Applicant"];
+                    edu.Major = (string)reader["Major"];
+                    edu.StartDate = (DateTime)reader["Start_Date"];
+
+                    applicantEducations[index] = edu;
+                    index++;
+                }
+
+            }
+
+        }
+    }
+
+    class ApplicantEducation
+    {
+        public Guid Id;
+        public Guid Applicant;
+        public string Major;
+        public DateTime StartDate;
+    }
+}
